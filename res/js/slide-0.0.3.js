25c8ddd866b4600312f019978c192f637ef9625e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-26 23:44:45 +0000

    1.1.5 +++ #45846
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_npzch/trunk@71647 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/slide-0.0.3.js
+++ res/js/slide-0.0.3.js
@@ -21,12 +21,15 @@ $( document ).ready( function( )
       // Beende das Durchblättern nach maxLoops Durchläufen
     if( curLoop >= maxLoops )
     {
-      alert(  "Die maximale Anzahl von " + maxLoops + " Durchläufen ist erreicht.\n" +
-              "\n" +
-              "Wenn die Tabs länger automatisch wechseln sollen, erhöhe bitte den Wert von maxLoops.\n" +
-              "maxLoops hat jetzt den Wert: " + maxLoops + ".\n" +
-              "Das Javascript befindet sich im TYPO3-Backend auf dieser Seite."
-            );
+      if( t3browserAlert )
+      {
+        alert(  "Die maximale Anzahl von " + maxLoops + " Durchläufen ist erreicht.\n" +
+                "\n" +
+                "Wenn die Tabs länger automatisch wechseln sollen, erhöhe bitte den Wert von maxLoops.\n" +
+                "maxLoops hat jetzt den Wert: " + maxLoops + ".\n" +
+                "Das Javascript befindet sich im TYPO3-Backend auf dieser Seite."
+              );
+      }
       return;
     }
       // Beende das Durchblättern nach maxLoops Durchläufen
@@ -37,16 +40,19 @@ $( document ).ready( function( )
         // ABBRUCH: FEHLER. Tab (Id) ist nicht vorhanden
       if( ! $( "#c###TT_CONTENT.UID###-list-tab-11081302" ).length )
       {
-        alert(  "FEHLER: Der Tab mit der Id #c###TT_CONTENT.UID###-list-tab-11081302 ist nicht vorhanden.\n" +
-                "\n" +
-                "Das kannst Du tun:\n" +
-                "* Diese Seite im TYPO3-Backend enthält ein Javascript.\n" +
-                "* Prüfe in dem Skript bitte die Variable ###TT_CONTENT.UID###.\n" +
-                "  ###TT_CONTENT.UID### hat jetzt den Wert ###TT_CONTENT.UID###.\n" +
-                "  ###TT_CONTENT.UID### muss den Wert der Id des Browser-Plugins haben.\n" +
-                "* Die andere Nummer im Tab ist die Id der View im TypoScript.\n" +
-                "  Die View bzw. die Id muss im TypoScript vorhanden sein."
-             );
+        if( t3browserAlert )
+        {
+          alert(  "FEHLER: Der Tab mit der Id #c###TT_CONTENT.UID###-list-tab-11081302 ist nicht vorhanden.\n" +
+                  "\n" +
+                  "Das kannst Du tun:\n" +
+                  "* Diese Seite im TYPO3-Backend enthält ein Javascript.\n" +
+                  "* Prüfe in dem Skript bitte die Variable ###TT_CONTENT.UID###.\n" +
+                  "  ###TT_CONTENT.UID### hat jetzt den Wert ###TT_CONTENT.UID###.\n" +
+                  "  ###TT_CONTENT.UID### muss den Wert der Id des Browser-Plugins haben.\n" +
+                  "* Die andere Nummer im Tab ist die Id der View im TypoScript.\n" +
+                  "  Die View bzw. die Id muss im TypoScript vorhanden sein."
+              );
+        }
         return;
       }
         // ABBRUCH: FEHLER. Tab (Id) ist nicht vorhanden
